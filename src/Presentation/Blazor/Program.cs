b1e411a5c1034388ca45776c7dc8cc5457fd2a74
Cannbayazit <bayazitdogancan@gmail.com>
2023-06-03 18:36:44 +0300

    update addnews page

--- src/Presentation/Blazor/Program.cs
+++ src/Presentation/Blazor/Program.cs
@@ -9,4 +9,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<TooltipService>();
 await builder.Build().RunAsync();
